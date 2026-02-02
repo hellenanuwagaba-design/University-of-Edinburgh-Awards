2c56f6b28485cd797d29d124c37c531966bb32cd
hellenanuwagaba-design <hellenanuwagaba@gmail.com>
2026-02-02 21:26:53 +0300

    Update script.js
    Edinburgh Award

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const toggleBtn = document.getElementById('toggleOther');
-  const otherSection = document.getElementById('other-opportunities');
+const toggleOtherBtn = document.getElementById('toggleOther');
+const otherSection = document.getElementById('other-opportunities');
 
-  toggleBtn.addEventListener('click', () => {
-    otherSection.classList.toggle('hidden');
-    if(!otherSection.classList.contains('hidden')) {
-      toggleBtn.textContent = "Hide Opportunities";
-    } else {
-      toggleBtn.textContent = "Other Opportunities";
-    }
-  });
+const exploreBtn = document.querySelector('.explore-btn');
+const mainSection = document.getElementById('opportunities');
+
+// Toggle Other Opportunities
+toggleOtherBtn.addEventListener('click', () => {
+  otherSection.classList.toggle('hidden');
+  if(!otherSection.classList.contains('hidden')) {
+    toggleOtherBtn.textContent = "Hide Opportunities";
+  } else {
+    toggleOtherBtn.textContent = "Other Opportunities";
+  }
+});
+
+// Toggle Main Opportunities
+exploreBtn.addEventListener('click', () => {
+  mainSection.classList.toggle('hidden');
+  if(!mainSection.classList.contains('hidden')) {
+    exploreBtn.textContent = "Hide Opportunities";
+  } else {
+    exploreBtn.textContent = "Explore Now";
+  }
+});
